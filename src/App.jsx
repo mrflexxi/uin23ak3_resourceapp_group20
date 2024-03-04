d39ae028979b63a6a3308af023d0086fa4592af1
ApolloDesktop <apollogamestuff@gmail.com>
2024-03-04 14:13:19 +0100

    Exporterer 'resources' fra ressurser.js & sjekker tilkobling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import resources from './assets/ressurser.js'
 import './App.css'
 
 function App() {
 
+  console.log(resources)
+
   return (
-    ""
+    <>
+      <div id="container">
+        <nav>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>React</li>
+            <li>Headless-CMS</li>
+          </ul>
+        </nav>
+        <div id="container">
+          <h1>HTML</h1>
+          <p>Content</p>
+        </div>
+      </div>
+    </>
   )
 }
 
