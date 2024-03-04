ba90859a6ba3a25c9ff873711f9394152f3d5d5e
ApolloDesktop <apollogamestuff@gmail.com>
2024-03-04 14:20:57 +0100

    Ferdig struktur for init Layout-branch

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,28 @@ function App() {
 
   return (
     <>
-      <div id="nav-cont">
-        <nav>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>React</li>
-            <li>Headless-CMS</li>
-          </ul>
-        </nav>
-        <div id="container">
-          <h1>HTML</h1>
-          <p>Content</p>
+      <div id="container">
+        <div id="nav-cont">
+          <nav>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Headless-CMS</li>
+            </ul>
+          </nav>
+          <div id="content-cont">
+            <h1>HTML</h1>
+            <p></p>
+            <div id="links-cont">
+              <ul>
+                <li>LINK1</li>
+                <li>LINK2</li>
+                <li>LINK3</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
